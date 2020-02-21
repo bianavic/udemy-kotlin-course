3d538bf7af5ace02fe19e24f5c9c80e5c34d08e5
bianavic <bianavic@gmail.com>
2020-02-21 10:24:51 -0300

    variables and values

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -1,4 +1,10 @@
 fun main(args: Array<String>) {
+    // var = mutable
+    // val = immutable
+    var question1 = 12%5
+    question1 = question1 + 2 // == question1 = 12%5 + 2
 
-    println("Hello There") // fun name + arguments
+    val question2 = 10*5
+
+    println((question1)+question2) // fun name + arguments
 }
