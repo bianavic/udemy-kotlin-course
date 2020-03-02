68fa574e6552357f76b8a70a11f4cdbee76ed15c
bianavic <bianavic@gmail.com>
2020-03-02 13:14:27 -0300

    while loop

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -1,24 +1,15 @@
 fun main(args: Array<String>) {
-    // lists are only readble. cant add, neither delete...
-//    val bestFriends = listOf<String>("Ryan", "Ted", "Jan", "Dylan")
-//    for(friend in bestFriends) {
-//        println(friend)
-
-    // array lists
-//    val bestFriends = arrayListOf<String>("Ryan", "Ted", "Jan", "Dylan")
-//    // bestFriends.add("Mike") // add to the end / this appends
-//    bestFriends.add(1, "Mike") // add this to a index / it is pushed to first index
-//    for(friend in bestFriends) {
-//        println(friend)
-//    }
-
     val bestFriends = arrayListOf<String>("Ryan")
-    println("Who is your best friend?")
+    var bestFriendInput: String = "Nothing"
+    while(bestFriendInput != "") {
 
-    val bestFriendInput = readLine()
-    bestFriends.add(bestFriendInput!!)
-    for(friend in bestFriends) {
-        println(friend)
+        println("Who is your best friend?: ")
+
+        bestFriendInput= readLine()!!
+        bestFriends.add(bestFriendInput)
+        for(friend in bestFriends) {
+            println(friend)
+        }
+
     }
-
 }
