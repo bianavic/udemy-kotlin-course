1ce82c4144ac64205f213b6491dbbc25574f20b8
bianavic <bianavic@gmail.com>
2020-03-02 10:42:32 -0300

    list of friends

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -1,15 +1,6 @@
 fun main(args: Array<String>) {
-//    val names = listOf<String>("Ted", "Billy", "Ryan")
-//    println("Hello, " + names[0])
-//    println("Hello, " + names[1])
-//    println("Hello, " + names[2])
-
-    // for loop
-    val names = listOf<String>("Ted", "Billy", "Ryan")
-    var numOfTimesLooped = 0
-    for(name in names) {
-        println("Hello, " + name)
-        println("Hello, " + numOfTimesLooped)
-        numOfTimesLooped = numOfTimesLooped + 1
+    val bestFriends = listOf<String>("Ryan", "Ted", "Jan", "Dylan")
+    for(friend in bestFriends) {
+        println(friend)
     }
 }
