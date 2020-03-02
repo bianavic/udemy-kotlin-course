a28bfea955245a17e77be5986374724cc818ac7f
bianavic <bianavic@gmail.com>
2020-03-02 14:08:06 -0300

    deleting friends form our best friends list 2/2

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -3,29 +3,28 @@
         var bestFriendInput: String = "Nothing"
         var deleteBestFriendInput: String = "Nothing"
         var optionPicked: String = "1"
+
         while(optionPicked != "3") {
+            bestFriendInput = "Nothing"
             println("1. Add best friend")
             println("2. Delete best friend")
             println("3. Exit")
             optionPicked = readLine()!!
 
             if(optionPicked == "1") {
-
-            while(bestFriendInput != "") {
-
-                println("Who is your best friend?: ")
-
-                bestFriendInput = readLine()!!
-                bestFriends.add(bestFriendInput)
-                for (friend in bestFriends) {
-                    println(friend)
-                }
+                while(bestFriendInput != "") {
+                    println("Who is your best friend?: ")
+                    bestFriendInput = readLine()!!
+                    bestFriends.add(bestFriendInput)
             }
-            } else if(optionPicked == "2") {
-                print("Name of Best Friend?: ")
+
+        } else if(optionPicked == "2") {
+                print("What index isn't your best friend anymore?: ")
                 deleteBestFriendInput = readLine()!!
-                bestFriends.remove(deleteBestFriendInput)
-
-            }
+                bestFriends.removeAt(deleteBestFriendInput.toInt())
+        }
+        for (friend in bestFriends) {
+            println(friend)
         }
     }
+}
