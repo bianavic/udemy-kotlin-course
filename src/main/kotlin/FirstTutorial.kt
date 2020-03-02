5a79585bec991de265f0b741a85d6743f63bd1a8
bianavic <bianavic@gmail.com>
2020-03-02 10:38:55 -0300

    for loops

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -1,7 +1,15 @@
 fun main(args: Array<String>) {
-    val names = listOf<String>("Ted", "Billy", "Ryan")
-    println("name: " + names[0])
-    println("name: " + names.get(1))
-    println("Is list Empty?: " + names.isEmpty())
+//    val names = listOf<String>("Ted", "Billy", "Ryan")
+//    println("Hello, " + names[0])
+//    println("Hello, " + names[1])
+//    println("Hello, " + names[2])
 
+    // for loop
+    val names = listOf<String>("Ted", "Billy", "Ryan")
+    var numOfTimesLooped = 0
+    for(name in names) {
+        println("Hello, " + name)
+        println("Hello, " + numOfTimesLooped)
+        numOfTimesLooped = numOfTimesLooped + 1
+    }
 }
