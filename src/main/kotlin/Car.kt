313e67eceae52c6b5100d9d2e331571ad861bcdd
bianavic <bianavic@gmail.com>
2020-03-03 11:45:58 -0300

    private variables

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,7 +1,15 @@
 class Car {
     var carName = "Toyota" // VARIAVEL GLOBAL == pertence a uma CLASS LEVEL
+    private var vinNumber = 1234
 
+    fun showVinNumber(): Int {
+        // permitimos que vejamos vinNumber desta funcao,
+        // mas agora retorna o VALOR ao inves da VARIAVEL
+        // esta é a unica forma de acessar a funcao
+        return vinNumber
+    }
     fun makeNoise(): String {
+
         return "Vruuuuuummmmm"
     }
 }
--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -1,6 +1,7 @@
     fun main(args: Array<String>) {
         // criado 1 OBJETO car. estamos chamando a CLASSE CARRO, estamos criando um POINTER
         val car = Car() // o parenteses diz: me dë o carro e todos os seus parametros
+        car.showVinNumber()
         println("carName: " + car.carName) // chamando variaveis
         println("carNoise: " + car.makeNoise()) // chamando funcoes
 }
