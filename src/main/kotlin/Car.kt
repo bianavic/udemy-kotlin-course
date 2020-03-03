27b1be991cb6b163f6babdf452c7e12ef3347b15
bianavic <bianavic@gmail.com>
2020-03-03 11:35:13 -0300

    classes and objects

---
+++ src/main/kotlin/Car.kt
@@ -0,0 +1,7 @@
+class Car {
+    var carName = "Toyota" // VARIAVEL GLOBAL == pertence a uma CLASS LEVEL
+
+    fun makeNoise(): String {
+        return "Vruuuuuummmmm"
+    }
+}
--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -1,23 +1,6 @@
     fun main(args: Array<String>) {
-        displayGreeting()
-        val name: String? = readLine()
-        val userGreeting: String = displayName(name)
-        println(userGreeting)
-
-        val answer = doComplexMathProblem(4, 5)
-        println("answer: $answer")
+        // criado 1 OBJETO car. estamos chamando a CLASSE CARRO, estamos criando um POINTER
+        val car = Car() // o parenteses diz: me dë o carro e todos os seus parametros
+        println("carName: " + car.carName) // chamando variaveis
+        println("carNoise: " + car.makeNoise()) // chamando funcoes
 }
-
-    fun displayGreeting() {
-        println("Hi, my name is Kotlin.")
-        println("I'm a computer.")
-        println("What's your name?")
-    }
-
-    fun displayName(name: String?): String {
-        return "Your name is $name"
-    }
-
-    fun doComplexMathProblem(num1: Int, num2: Int): Int {
-        return num1 + num2
-    }
