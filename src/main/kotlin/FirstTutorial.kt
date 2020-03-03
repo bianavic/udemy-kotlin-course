a9e03728d46bc5b6876db146ccd782476300225f
bianavic <bianavic@gmail.com>
2020-03-03 13:41:03 -0300

    data classes

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -1,14 +1,6 @@
-    fun main(args: Array<String>) {
-        // criado 1 OBJETO car. estamos chamando a CLASSE CARRO, estamos criando um POINTER
-        val car = Car("Ford", 1212) // o parenteses diz: me dë o carro e todos os seus parametros
-        // CONSTRUCTOR == create a OBJECT that is a car and you pass in a default (TOYOTA), parameter,
-        // like a FUNCTION or you pass it to the CLASS
-        println("carName: " + car.carName) // chamando variaveis
-        println("carName: " + car.vinNumber)
-        println("carNoise: " + car.makeNoise()) // chamando funcoes
-        car.doStuff()
-        val tractor = Tractor()
-        tractor.makeNoise()
-        tractor.roamAroundCrops()
-        tractor.doStuff()
+fun main(args: Array<String>) {
+    val person: Person = Person("Ted", 72, 60)
+        println("personName: " + person.name)
+        println("personAge: " + person.age)
+        println("personWeight: " + person.weight)
 }
---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,3 @@
+// data class pros: just to hold variables
+// data class cons: cant have functionality. its used to hold variables | to have functionality use class
+data class Person(var name: String, var age: Int, var weight: Int)
