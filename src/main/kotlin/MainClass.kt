1e25c26803f242e81780f11d25aecec9ca1ad28d
bianavic <bianavic@gmail.com>
2020-03-04 15:57:40 -0300

    using != null

--- src/main/kotlin/MainClass.kt
+++ src/main/kotlin/MainClass.kt
@@ -7,48 +7,53 @@ var usersChoice: Int = WITHDRAW
 fun main(args: Array<String>) {
     val listOfbanks = listOf(Bank(), Bank(), null)
     listOfbanks.forEach { item ->
-        val money = item!!.moneyInBank
-        println("money: $money")
-    }
-    // val bank: Bank = Bank()
-    // doBankActionFromUserInput(bank)
-}
+        if (item != null) {
 
-fun doBankActionFromUserInput(bank: Bank) {
-    while (usersChoice != EXIT) {
-        greetUserWithChoices()
-        displayAmountInBank(bank)
-        usersChoice = readLine()!!.toInt()
-        if(usersChoice == DEPOSIT) {
-            val depositAmount = getDepositAmountFromUser()
-            bank.deposit(depositAmount)
-        } else if (usersChoice == WITHDRAW) {
-            val withdrawAmount = getWithdrawAmountFromUser()
-            bank.withdraw(withdrawAmount)
+            val money = item.moneyInBank
+            println("money: $money")
+        } else {
+            println("This is null")
         }
+        // val bank: Bank = Bank()
+        // doBankActionFromUserInput(bank)
     }
 
-}
+//fun doBankActionFromUserInput(bank: Bank) {
+//    while (usersChoice != EXIT) {
+//        greetUserWithChoices()
+//        displayAmountInBank(bank)
+//        usersChoice = readLine()!!.toInt()
+//        if(usersChoice == DEPOSIT) {
+//            val depositAmount = getDepositAmountFromUser()
+//            bank.deposit(depositAmount)
+//        } else if (usersChoice == WITHDRAW) {
+//            val withdrawAmount = getWithdrawAmountFromUser()
+//            bank.withdraw(withdrawAmount)
+//        }
+//    }
+//
+//}
 
-fun greetUserWithChoices() {
-    println("Choose what you want. ")
-    println("1: Withdraw")
-    println("2: Deposit")
-    println("3: Exit")
-}
+    fun greetUserWithChoices() {
+        println("Choose what you want. ")
+        println("1: Withdraw")
+        println("2: Deposit")
+        println("3: Exit")
+    }
 
-fun displayAmountInBank(bank: Bank) {
-    println("Current amount of money is: ${bank.moneyInBank}")
-}
+    fun displayAmountInBank(bank: Bank) {
+        println("Current amount of money is: ${bank.moneyInBank}")
+    }
 
-fun getDepositAmountFromUser(): Double {
-    print("How much would you like to deposit?")
-    val depositAmount = readLine()!!.toDouble()
-    return depositAmount
-}
+    fun getDepositAmountFromUser(): Double {
+        print("How much would you like to deposit?")
+        val depositAmount = readLine()!!.toDouble()
+        return depositAmount
+    }
 
-fun getWithdrawAmountFromUser(): Double {
-    print("How much would you like to withdraw?")
-    val withdrawAmount = readLine()!!.toDouble()
-    return withdrawAmount
+    fun getWithdrawAmountFromUser(): Double {
+        print("How much would you like to withdraw?")
+        val withdrawAmount = readLine()!!.toDouble()
+        return withdrawAmount
+    }
 }
